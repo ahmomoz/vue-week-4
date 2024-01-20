857afac87d61095c46dfc25ddabdfcf04964b58a
MY <x5426854268@gmail.com>
2024-01-21 00:56:04 +0800

    update

--- JS/products.js
+++ JS/products.js
@@ -1,18 +1,28 @@
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
+
 let myProductModal = '';
 let myDeleteModal = '';
-const App = {
+
+import CardPagination from './CardPagination.js' // 分頁元件
+import CardProductModel from './CardProductModel.js' // 新增編輯產品Model元件
+import CardDeleteModel from './CardDeleteModel.js' // 刪除Model元件
+
+
+// 主元件
+const app = createApp({
     data() {
-    return {
-        sortBy : "default",
-        ascending : true,
-        products: [],
-        productsDetail: {},
-        tempProduct: {
-            imagesUrl: [],
-        },
-        url : 'https://ec-course-api.hexschool.io/v2',
-        path : "ahmomoz",
-    }
+        return {
+            sortBy: "default",
+            ascending: true,
+            products: [],
+            productsDetail: {},  
+            tempProduct: {
+                imagesUrl: [],
+            },
+            url: 'https://ec-course-api.hexschool.io/v2',
+            path: "ahmomoz",
+            pagination: {} ,
+        }
     },
     methods: {
         checkAdmin(){ // 檢驗身分函式
@@ -20,18 +30,20 @@ const App = {
             .then(res=>{
                 this.getProductsList(); // 進行取得產品列表函式
             })
-            .catch(error=>{
+            .catch(err=>{
                 alert("身分驗證錯誤，將跳轉至登入頁")
                 window.location.href = "login.html"; // 跳轉至登入頁
             })
         },
-        getProductsList(){  // 取得產品資料
-            axios.get(`${this.url}/api/${this.path}/admin/products`)
+        getProductsList(page=1){  // 取得產品資料
+            axios.get(`${this.url}/api/${this.path}/admin/products?page=${page}`)
             .then(res=>{
-                this.products = [...res.data.products]
+                const {products,pagination} = res.data ;
+                this.products = products;
+                this.pagination = pagination;
             })
-            .catch(error=>{
-                alert("商品列表取得發生錯誤")
+            .catch(err=>{
+                alert(err.response.data.message);
             })
         },
         openProductModal(product,method){           // 開啟Modal
@@ -48,43 +60,21 @@ const App = {
                 myProductModal.show();
             }
         },
-        updateProduct(){    // 新增產品函式
-            let url = `${this.url}/api/${this.path}/admin/product`;
-            let way = "post";
-            if(this.tempProduct.id){
-                url = `${this.url}/api/${this.path}/admin/product/${this.tempProduct.id}`;
-                way = "put";
-            };
-            axios[way](url ,{data:this.tempProduct})
-            .then(res=>{
-                alert("更新成功");
-                myProductModal.hide();  // 更新動作結束，關掉modal
-                this.getProductsList(); // 再次取得產品列表，重新渲染頁面
-            })
-            .catch(error=>{
-                alert("更新失敗，請確認資料是否正確");
-            })
-        },
-        deleteProduct(){   // 刪除產品函式
-            axios.delete(`${this.url}/api/${this.path}/admin/product/${this.tempProduct.id}`)
-            .then(res=>{
-                alert("商品刪除成功");
-                this.tempProduct={
-                    imagesUrl: [],
-                };
-                myDeleteModal.hide();   // 刪除動作結束，關掉modal
-                this.getProductsList(); // 再次取得產品列表，重新渲染頁面
-            })
-            .catch(error=>{
-                console.dir(error);
-                alert("商品刪除失敗");
-            })
-        },
-        addImg(){ // 新增多張圖片
-            this.tempProduct.imagesUrl.push('');
-        },
-        deleteImg(){  // 清除多張圖片
+        deleteImg(item){  // 清除圖片
             this.tempProduct.imagesUrl=[''];
+        },
+        imgUrlUpdate(url){
+            if(this.tempProduct.imagesUrl[0]===''){
+                this.tempProduct.imagesUrl[0] = url;
+            }else{
+                this.tempProduct.imagesUrl.push(url);
+            }
+        },
+        hideModal() {
+            myProductModal.hide();
+        },
+        hideDeleteModal() {
+            myDeleteModal.hide();
         },
     },
     computed:{
@@ -96,12 +86,18 @@ const App = {
         }
     },
     mounted() {
-        myProductModal = new bootstrap.Modal(document.getElementById('productModal'));
-        myDeleteModal = new bootstrap.Modal(document.getElementById('delProductModal'));
         // 取得token
         const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*\=\s*([^;]*).*$)|^.*$/,"$1");
         axios.defaults.headers.common['Authorization'] = token;
-        this.checkAdmin(); //進行身分驗證函式   
+        this.checkAdmin(); //進行身分驗證函式 
+        myProductModal = new bootstrap.Modal(document.getElementById('productModal'));
+        myDeleteModal = new bootstrap.Modal(document.getElementById('delProductModal'));
+    },
+    components:{
+        CardPagination,
+        CardProductModel,
+        CardDeleteModel
     }
-};
-Vue.createApp(App).mount('#app');
+});
+
+app.mount('#app');
